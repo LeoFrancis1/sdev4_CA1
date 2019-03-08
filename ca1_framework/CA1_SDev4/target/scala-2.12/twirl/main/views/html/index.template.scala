6f1179a3d3b8680ccf7cd993c83086dc774368cf
Fleming-James-J <fleming.james.joseph@gmail.com>
2019-03-08 12:09:04 +0000

    Index page

--- ca1_framework/CA1_SDev4/target/scala-2.12/twirl/main/views/html/index.template.scala
+++ ca1_framework/CA1_SDev4/target/scala-2.12/twirl/main/views/html/index.template.scala
@@ -22,33 +22,113 @@ import play.mvc.Http.Context.Implicit._
 import play.data._
 import play.core.j.PlayFormsMagicForJava._
 
-object index extends _root_.play.twirl.api.BaseScalaTemplate[play.twirl.api.HtmlFormat.Appendable,_root_.play.twirl.api.Format[play.twirl.api.HtmlFormat.Appendable]](play.twirl.api.HtmlFormat) with _root_.play.twirl.api.Template1[String,play.twirl.api.HtmlFormat.Appendable] {
+object index extends _root_.play.twirl.api.BaseScalaTemplate[play.twirl.api.HtmlFormat.Appendable,_root_.play.twirl.api.Format[play.twirl.api.HtmlFormat.Appendable]](play.twirl.api.HtmlFormat) with _root_.play.twirl.api.Template0[play.twirl.api.HtmlFormat.Appendable] {
 
-  /*
- * This template takes a single argument, a String containing a
- * message to display.
- */
-  def apply/*5.2*/(message: String):play.twirl.api.HtmlFormat.Appendable = {
+  /**/
+  def apply():play.twirl.api.HtmlFormat.Appendable = {
     _display_ {
       {
 
 
-Seq[Any](format.raw/*6.1*/("""
-"""),format.raw/*11.4*/("""
-"""),_display_(/*12.2*/main("Welcome to Play")/*12.25*/ {_display_(Seq[Any](format.raw/*12.27*/("""
+Seq[Any](format.raw/*1.1*/("""<!DOCTYPE html>
+<html>
+    <head>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta charset="UTF-8"/>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<style>
+	    .footer """),format.raw/*8.14*/("""{"""),format.raw/*8.15*/(""" """),format.raw/*8.16*/("""font-weight: bold; color: white; background-color: black; """),format.raw/*8.74*/("""}"""),format.raw/*8.75*/("""
+	    """),format.raw/*9.6*/(""".sidecol """),format.raw/*9.15*/("""{"""),format.raw/*9.16*/(""" """),format.raw/*9.17*/("""font-weight: bold; background-color: steelblue; """),format.raw/*9.65*/("""}"""),format.raw/*9.66*/("""
+	    """),format.raw/*10.6*/(""".sidecol h2 """),format.raw/*10.18*/("""{"""),format.raw/*10.19*/(""" """),format.raw/*10.20*/("""font-weight: bold; """),format.raw/*10.39*/("""}"""),format.raw/*10.40*/("""
+	    """),format.raw/*11.6*/(""".midcol h1 """),format.raw/*11.17*/("""{"""),format.raw/*11.18*/(""" """),format.raw/*11.19*/("""margin-top: 2em; text-align: center; font-weight: bold; """),format.raw/*11.75*/("""}"""),format.raw/*11.76*/("""
+	    """),format.raw/*12.6*/(""".midcol h3 """),format.raw/*12.17*/("""{"""),format.raw/*12.18*/(""" """),format.raw/*12.19*/("""text-align: center; color: steelblue; """),format.raw/*12.57*/("""}"""),format.raw/*12.58*/("""
+	    """),format.raw/*13.6*/(""".redtext """),format.raw/*13.15*/("""{"""),format.raw/*13.16*/(""" """),format.raw/*13.17*/("""color: red; """),format.raw/*13.29*/("""}"""),format.raw/*13.30*/("""
+	"""),format.raw/*14.2*/("""</style>
+	<title>JMC</title>
+    </head>
+    <body>
+	<nav class="navbar navbar-inverse">
+	    <div class="container-fluid">
+		<div class="navbar-header">
+		    <a class="navbar-brand" href="#">Jupiter Mining Corporation</a>
+		</div>
+		<ul class="nav navbar-nav">
+		    <li class="active"><a href="#">Home</a></li>
+		    <li><a href="#">Projects</a></li>
+		    <li><a href="#">Contact</a></li>
+            <li><a href="#">Account</a></li>
+		</ul>
+	    </div>
+	</nav>
 
-    """),format.raw/*17.8*/("""
-    """),_display_(/*18.6*/welcome(message, style = "java")),format.raw/*18.38*/("""
+	<div class="container-fluid">
+	    <div class="row">
+		<div class="col-sm-3">
+		    <div class="well sidecol">
+			<h2>News</h2>
+			<h3>Employee notice:</h3>
+			<p>Make sure to report to your website administrator if you experience any issues with this website or your account</p>
+		    </div>
+		</div>
+		<div class="col-sm-6">
+		    <div class="midcol">
+			<h1>Welcome to the Jupiter Mining Corporation<br>Website</b></h1>
+            
+			
+		    </div>
+		</div>
+		<div class="col-sm-3">
+		    <div class="well sidecol">
+			<h2>Next product launch!</h2>
+			<h3>Countdown to our next product release!</h3>
+			<p id="demo"></p>
 
-""")))}),format.raw/*20.2*/("""
+    <script>
+        var countDownDate = new Date("Jan 5, 2021 15:37:25").getTime();
+
+        var x = setInterval(function() """),format.raw/*57.40*/("""{"""),format.raw/*57.41*/("""
+
+        """),format.raw/*59.9*/("""var now = new Date().getTime();
+    
+        var distance = countDownDate - now;
+    
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+        document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+         + minutes + "m " + seconds + "s ";
+    
+        if (distance < 0) """),format.raw/*71.27*/("""{"""),format.raw/*71.28*/("""
+         """),format.raw/*72.10*/("""clearInterval(x);
+         document.getElementById("demo").innerHTML = "EXPIRED";
+             """),format.raw/*74.14*/("""}"""),format.raw/*74.15*/("""
+                """),format.raw/*75.17*/("""}"""),format.raw/*75.18*/(""", 1000);
+                </script>
+		    </div>
+		</div>
+	    </div>
+	    <div class="row">
+		<div class="col-sm-12">
+		    <div class="well footer">
+			Website created by Kryten
+		    </div>
+		</div>
+	    </div>
+	</div>
+    </body>
+</html>
+
+
 """))
       }
     }
   }
 
-  def render(message:String): play.twirl.api.HtmlFormat.Appendable = apply(message)
+  def render(): play.twirl.api.HtmlFormat.Appendable = apply()
 
-  def f:((String) => play.twirl.api.HtmlFormat.Appendable) = (message) => apply(message)
+  def f:(() => play.twirl.api.HtmlFormat.Appendable) = () => apply()
 
   def ref: this.type = this
 
@@ -57,11 +137,11 @@ Seq[Any](format.raw/*6.1*/("""
 
               /*
                   -- GENERATED --
-                  DATE: Fri Mar 08 10:51:53 GMT 2019
+                  DATE: Fri Mar 08 12:08:01 GMT 2019
                   SOURCE: /home/wdd/sDev4CA1/sdev4_CA1/ca1_framework/CA1_SDev4/app/views/index.scala.html
-                  HASH: 9040cdc6391df1f5c9d9a05a763073d6fd21d0f1
-                  MATRIX: 1037->95|1148->113|1176->308|1204->310|1236->333|1276->335|1309->464|1341->470|1394->502|1427->505
-                  LINES: 31->5|36->6|37->11|38->12|38->12|38->12|40->17|41->18|41->18|43->20
+                  HASH: cea1a5c96b3f80a69f560aa05305fd7c177ea8e8
+                  MATRIX: 1030->0|1312->255|1340->256|1368->257|1453->315|1481->316|1513->322|1549->331|1577->332|1605->333|1680->381|1708->382|1741->388|1781->400|1810->401|1839->402|1886->421|1915->422|1948->428|1987->439|2016->440|2045->441|2129->497|2158->498|2191->504|2230->515|2259->516|2288->517|2354->555|2383->556|2416->562|2453->571|2482->572|2511->573|2551->585|2580->586|2609->588|3856->1807|3885->1808|3922->1818|4493->2361|4522->2362|4560->2372|4683->2467|4712->2468|4757->2485|4786->2486
+                  LINES: 33->1|40->8|40->8|40->8|40->8|40->8|41->9|41->9|41->9|41->9|41->9|41->9|42->10|42->10|42->10|42->10|42->10|42->10|43->11|43->11|43->11|43->11|43->11|43->11|44->12|44->12|44->12|44->12|44->12|44->12|45->13|45->13|45->13|45->13|45->13|45->13|46->14|89->57|89->57|91->59|103->71|103->71|104->72|106->74|106->74|107->75|107->75
                   -- GENERATED --
               */
           
