4f8da3c5fef4b03bf6b62b337ad0fff2556768a1
Fleming-James-J <fleming.james.joseph@gmail.com>
2019-03-16 20:53:24 +0000

    small syntax errors in addProject.scala, still route errors to fix

--- ca1_framework/CA1_SDev4/app/models/Project.java
+++ ca1_framework/CA1_SDev4/app/models/Project.java
@@ -36,6 +36,15 @@ public class Project extends Model {
         this.allocatedBudget = allocatedBudget;
     }
 
+    public void setId(Long id)
+    {
+        this.id = id;
+    }
+
+    public Long getId()
+    {
+        return this.id;
+    }
     public void setProjectName(String projectName)
     {
         this.projectName = projectName;
