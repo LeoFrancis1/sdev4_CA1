399a0124dd7d3e96f72350501e37ce9398a4908e
Leo Francis <X00145916@outlook.com>
2019-03-16 16:32:08 +0000

    Project scala page with erros

--- ca1_framework/CA1_SDev4/target/scala-2.12/routes/main/controllers/routes.java
+++ ca1_framework/CA1_SDev4/target/scala-2.12/routes/main/controllers/routes.java
@@ -1,6 +1,6 @@
 // @GENERATOR:play-routes-compiler
-// @SOURCE:/home/wdd/sdev4ca1v2/sdev4_CA1/ca1_framework/CA1_SDev4/conf/routes
-// @DATE:Sat Mar 16 14:12:11 GMT 2019
+// @SOURCE:/home/wdd/sdevca/sdev4_CA1/ca1_framework/CA1_SDev4/conf/routes
+// @DATE:Sat Mar 16 16:26:39 GMT 2019
 
 package controllers;
 
